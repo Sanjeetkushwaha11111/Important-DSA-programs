b1dd955398ff7620de4de3e45bf96689f44f119b
SanjeetKushwaha <sanjeetkushwaha1111gmailom>
2021-05-22 06:27:40 +0530

    Important DSA programs...

---
+++ oddeven.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+
+  std::cout << "Enter your rating out of 10 in thid IDE:\n" << '\n';
+  std::cin >> exp;
+  std::cout << "Wishing you a very good coding experience.\n Happy Coding" << '\n';
+  return 0;
+}
